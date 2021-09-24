7179378886b751e0da39bf509c044905587b30ba
piaocreative <johnpiao171019@gmail.com>
2021-09-25 00:45:54 +0800

    demo10-0925

--- js/main.js
+++ js/main.js
@@ -64,6 +64,55 @@ $( document ).ready( function() {
         ]
     });
 
+    $('.logos').slick({
+      dots: false,
+      arrows: false,
+      speed: 300,
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 5,
+            slidesToScroll: 1,
+          }
+        },
+
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+          }
+        },
+
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1
+          }
+        },
+
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+  });
+
 } );
 
 
@@ -71,8 +120,8 @@ $(".reviews").slick({
     centerMode: true,
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows: true,
-    dots:true,
+    arrows: false,
+    dots:false,
     lazyLoad: "ondemand",
     responsive: [
       {
@@ -89,6 +138,24 @@ $(".reviews").slick({
         }
       }
     ]
+  });
+  
+  $(".review-prev").click(function(e) {
+    e.preventDefault();
+    $('.reviews').slick('slickPrev');
+  })
+  
+  $(".review-next").click(function(e) {
+    e.preventDefault();
+    $('.reviews').slick('slickNext');
+  })
+
+  $(".reviews").on('init reInit afterChange', function(event, slick, currentSlide, nextSlide){
+      var i = (currentSlide ? currentSlide : 0);
+      var a = $(".indicator").children("a:eq("+i+")");
+      $(".active").removeClass();
+      a.addClass("active");
+      console.log(a);
   });
 
   $(".about-slick").slick({
